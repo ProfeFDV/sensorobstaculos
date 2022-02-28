948cf18d34cc7edd90877b6c3af925772156a0e6
Fran Diaz <100407685+ProfeFDV@users.noreply.github.com>
2022-02-28 19:16:14 +0100

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,7 @@
+basic.forever(function () {
+    if (pins.digitalReadPin(DigitalPin.P0) == 1) {
+        basic.showIcon(IconNames.Happy)
+    } else {
+        basic.showIcon(IconNames.Asleep)
+    }
+})
